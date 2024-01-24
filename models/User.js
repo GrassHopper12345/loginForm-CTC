3813f2fa881b3f5618ec5d35fa8e7d07cda1d6ef
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-24 14:53:05 -0600

    update to connections.js and models, updated api index.js

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            maxlength: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            maxlength: 50,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please enter a valid email address"
+            ],
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,11 +1,3 @@
-const { connect, connection } = require('mongoose');
+const User = require('./User');
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/.................PUT SOMETHING IN HERE................................';
-
-connect(connectionString), {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}
-
-module.exports = connection;
+module.exports =  User ;
