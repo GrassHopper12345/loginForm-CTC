67f3d72ef1c647f4e53da1241c47ca163561adb2
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-24 19:00:30 -0600

    update to server.js

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
+const path = require('path');
+const session = require('express-session');
 const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
 db.once('open', () => {
@@ -14,3 +37,7 @@ db.once('open', () => {
     console.log(`http://localhost:${PORT}`);
   });
 });
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
