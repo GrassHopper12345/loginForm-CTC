789e22cc4fceff462bbafe3da99b22b9e4e902ee
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-25 10:19:46 -0600

    update to dependencies

--- server.js
+++ server.js
@@ -12,18 +12,6 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const hbs = exphbs.create({ helpers });
 
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize,
-//     }),
-// };
-
-// app.use(session(sess));
-
 app.engine('handlebars', hbs.engine);
 
 app.use(express.urlencoded({ extended: true }));
@@ -38,6 +26,3 @@ db.once('open', () => {
   });
 });
 
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log('Now listening'));
-//   });
